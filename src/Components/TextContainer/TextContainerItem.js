c9dc7980ff1a81d46c9936d88b576fc90304803e
Bidex-03 <abdullahqaasim14@gmail.com>
2023-01-27 03:41:32 +0100

    updated the state

--- src/Components/TextContainer/TextContainerItem.js
+++ src/Components/TextContainer/TextContainerItem.js
@@ -4,25 +4,20 @@ import Card from "../UI/Card";
 import "./TextContainerItem.css";
 
 const TextContainer = (props) => {
-  const [heading, setHeading] = useState(props.heading);
+  const [heading, setHeading] = useState(
+    "The farmers will make more money. Your lunch will not be imported, cassava garri ewa and ehhh ehhhhnn. The farmer will make money, the dinner would be cassava eba ewa and everything."
+  );
 
-//   const changeText = () => {
-//     setHeading(
-//       "A town hall different from bala blu, blue blu bulaba. broom broom broom brooooooooom. Bala blu blue blu bulaba."
-//     );
   const changeText = () => {
-    
+    setHeading(
+      "A town hall different from bala blu, blue blu bulaba. broom broom broom brooooooooom. Bala blu blue blu bulaba."
+    );
   };
 
   return (
     <Card className="container">
       <Card className="inner-container">
-        <h2>
-          The farmers will make more money. Your lunch will not be imported,
-          cassava garri ewa and ehhh ehhhhnn. The farmer will make money, the
-          dinner would be cassava eba ewa and everything.
-        </h2>
-        {/* <h2>{props.heading}</h2> */}
+        <h2>{heading}</h2>
       </Card>
       <button className="button" onClick={changeText}>
         Change Text
