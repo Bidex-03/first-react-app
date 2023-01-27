f459efe6998909f5f4b2cf63ee6ee64daf8ae401
Bidex-03 <abdullahqaasim14@gmail.com>
2023-01-27 05:39:59 +0100

    made some changes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import Container from "./Components/TextContainer/Container";
 import TextContainerItem from "./Components/TextContainer/TextContainerItem";
 
 function App() {
-  const heading = [
-    {
-      id: "e1",
-      title:
-        "The farmers will make more money. Your lunch will not be imported, cassava garri ewa and ehhh ehhhhnn. The farmer will make money, the dinner would be cassava eba ewa and everything.",
-    },
-    {
-      id: "e2",
-      title:
-        "A town hall different from bala blu, blue blu bulaba. broom broom broom brooooooooom. Bala blu blue blu bulaba.",
-    },
-  ];
+  // const heading = [
+  //   {
+  //     id: "e1",
+  //     title:
+  //       "The farmers will make more money. Your lunch will not be imported, cassava garri ewa and ehhh ehhhhnn. The farmer will make money, the dinner would be cassava eba ewa and everything.",
+  //   },
+  //   {
+  //     id: "e2",
+  //     title:
+  //       "A town hall different from bala blu, blue blu bulaba. broom broom broom brooooooooom. Bala blu blue blu bulaba.",
+  //   },
+  // ];
   return (
     <div>
-      <TextContainerItem items={heading} />
+      <TextContainerItem/>
     </div>
   );
 }
